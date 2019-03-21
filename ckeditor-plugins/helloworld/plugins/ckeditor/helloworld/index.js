5992a6040c85a3518ab4721ed7aac1bedb8b1733
cl <claire.li@cloudcms.com>
2019-03-21 15:34:53 -0400

    test

--- ckeditor-plugins/helloworld/plugins/ckeditor/helloworld/index.js
+++ ckeditor-plugins/helloworld/plugins/ckeditor/helloworld/index.js
@@ -3,7 +3,6 @@ define(function(require, exports, module) {
     var CKEditorTools = require("ckeditor-tools");
 
     CKEditorTools.registerPlugin("helloworld", {
-        "filepath": "ckeditor-plugins/helloworld/plugins/ckeditor/helloworld/plugin.js",
         "module": module
     });
 
